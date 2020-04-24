ce3c00bba03aa0ff0da2565363b8eeab55a058c3
cicciozell <64215052+cicciozell@users.noreply.github.com>
2020-04-24 12:27:28 +0200

    Update seed2.groovy

--- seed2.groovy
+++ seed2.groovy
@@ -7,7 +7,7 @@ def jsonSlurper2 = new JsonSlurper()
 
 //update the filepath with the path of your job.
 
-def jobs = new BufferedReader(new InputStreamReader(new FileInputStream( "/var/lib/jenkins/workspace/testSd/seed2.json"),"UTF-8"))
+def jobs = new BufferedReader(new InputStreamReader(new FileInputStream( "/var/lib/jenkins/workspace/dslSeedWithJobGitJson/seed2.json"),"UTF-8"))
 data2 = jsonSlurper2.parse(jobs)
 def allSJObs = data2.jobs as Object
 data2.jobs.each
@@ -21,21 +21,21 @@ data2.jobs.each
         logRotator(5, 100, -1, -1)
         parameters {
             stringParam('Name', 'DefaultValue', 'This is a first parameter')
-            stringParam('Age', '30', 'This is a second parameter')
-          stringParam('test', '1233430', 'This is a second parameter')
+            stringParam('Age',  '30',           'This is a second parameter')
+            stringParam('test', '1233430',      'This is a second parameter')
         }
         label('master')
     steps {
         downstreamParameterized {
-            trigger('Project1') {
+            trigger('testDownstreamWith2Params') {
                 block {
                     buildStepFailure('FAILURE')
                     failure('FAILURE')
                     unstable('UNSTABLE')
                 }
                 parameters {
-                    predefinedProp('Name', '$Name')
-                    predefinedProp('Age', '$Age')
+                    predefinedProp('downstreamFirstParam', '$Name')
+                    predefinedProp('downstreamSecondParam', '$Age')
                }
             }
         }
